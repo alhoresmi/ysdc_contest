a673f98aaa60d4bbb76649e86d84b0b755dd0942
vladimir.k <alhoresmi@gmail.com>
2023-02-11 01:39:02 +0300

    made chains work. need to optimize search

--- main.py
+++ main.py
@@ -6,27 +6,42 @@ import random
 
 
 class Rover:
-    def __init__(self, pR, state, idx):
-        self.pR = pR
+    def __init__(self, x, y, state, idx):
+        self.x = x
+        self.y = y
+        self.pR = [self.x, self.y]
         self.state = state
         self.task = ""
         self.idx = idx
 
     def update(self):
-        if len(self.task) < 60:
-            self.task = self.task + 'S'*(60 - len(self.task))
-        print(self.task[:60])
-        dx, dy = route_to_xy(self.task[:60])
-        self.pR[0] += dx
-        self.pR[1] += dy
-        if (self.task[59] == 'S' or self.task[59] == 'P') and len(self.task) == 60:
-            self.state = 'S'
-            self.task = 'S'*60
+        global norders
+        if self.state != 'P':
+            # uncomment task print before sending to ya
+            #print(60*'S')
+            pass
         else:
-            self.state = 'P'
-            self.task = self.task[60:]
+            if len(self.task) < 60:
+                self.task = self.task + 'S'*(60 - len(self.task))
 
-        print(f"ROVER UPDATE {self.idx} {self.pR}")
+            # uncomment task print before sending to ya
+            #print(self.task[:60])
+
+            #dx, dy = route_to_xy(self.task[:60])
+            #self.pR[0] += dx
+            #self.pR[1] += dy
+            if (self.task[59] == 'S' or self.task[59] == 'P') and len(self.task) == 60:
+                self.state = 'S'
+                self.task = 'S'*60
+                norders += 1
+            else:
+                self.state = 'P'
+                self.task = self.task[60:]
+
+            #print(f"ROVER UPDATE {self.idx} {self.pR}")
+
+    def update_position(self, x, y):
+        self.pR = [x, y]
 
 
 class Order:
@@ -48,34 +63,37 @@ class Orders:
     def put(self, order):
         mdist = 2*N
         midx = 0
-        if self.empty_chains > 0:
-            for i in range(nrovers):
-                if self.chains[i] == [] and rvs[i].state != 'P':
-                    self.chains[i].append(order)
-                    self.empty_chains -= 1
-                    break
-        else:
-            i = 0
-            for chain in self.chains:
-                if chain != []:
-                    if dist(order.pT, chain[-1].pP) < mdist:
-                        mdist = dist(order.pT, chain[-1].pP)
-                        midx = i
-                i += 1
+        #print(f"PUT ORDER {self.empty_chains}")
+        for i in range(nrovers):
+            pt = self.chains[i][-1].pP if self.chains[i] != [] else rvs[i].pR 
+            dst = dist(order.pT, pt)
+            if dst < mdist:
+                mdist = dst
+                midx = i
+        if self.chains[midx] == []:
+            self.empty_chains -= 1
+        self.chains[midx].append(order)
 
-            self.chains[midx].append(order)
-
-    def pop(self, rover_idx):
-        o = self.chains[rover_idx].pop(0)
-        if self.chains[rover_idx] == []:
+    def pop(self, idx):
+        o = self.chains[idx].pop(0)
+        if self.chains[idx] == []:
             self.empty_chains += 1
         return o
 
-    def print_chains(self):
+    def print_chains(self, verbose=True):
+        print(f"empty_chains {self.empty_chains}")
         for i in range(nrovers):
-            print(f"chain {i}")
-            for o in self.chains[i]:
-                print(f"{o.pT}->{o.pP}")
+            print(f"chain {i} {len(self.chains[i])}")
+            if verbose:
+                for o in self.chains[i]:
+                    print(f"{o.pT}->{o.pP}")
+
+    def max_chain(self):
+        m = 0
+        for ch in self.chains:
+            if len(ch) > m:
+                m = len(ch)
+        return m
 
 
 class Map:
@@ -110,17 +128,20 @@ def setup():
 
 
 def how_much_rovers(N, max_tips, cost_c):
+    """
+    nrovers ~= sum(distances)/nT/60
+    """
     d = {
         '4_20_10': 1,
-        '128_500_1000': 5,
-        '180_500_5000': 10,
-        '384_3600_765432': 9, # 10
-        '1024_3600_555555': 25,
-        '1000_3600_131072': 7,
+        '128_500_1000': 5, #5
+        '180_500_5000': 8, #5
+        '384_3600_765432': 15, # 10
+        '1024_3600_555555': 25, #25
+        '1000_3600_131072': 10,
         '1000_3600_123456': 1,
-        '1000_3600_101010': 10,
-        '1000_3600_1500000': 1,
-        '1000_3600_1048576': 1
+        '1000_3600_101010': 1,
+        '1000_3600_1500000': 25,
+        '1000_3600_1048576': 50
     }
     return d[f"{N}_{max_tips}_{cost_c}"]
 
@@ -128,7 +149,7 @@ def how_much_rovers(N, max_tips, cost_c):
 def rovers_init(nrovers, N, max_tips, cost_c):
     rvs = []
     for i in range(nrovers):
-        rvs.append(Rover([2, 2], 'S', i))
+        rvs.append(Rover(2, 2, 'S', i))
 
     d = {
         '4_20_10': [1, 1],
@@ -152,8 +173,11 @@ def create_order(s):
     s = [int(v)-1 for v in s.strip().split(" ")]
     o = Order((s[0], s[1]), (s[2], s[3]))
     o.path = path_to_directions(search(city, o.pT, o.pP))
+    #o.path = "UUUDDD"
     o.length = len(o.path)
     o.time = o.length
+    #print(f"CREATE ORDER {o.pT}->{o.pP} {o.path}")
+
     return o
 
 
@@ -233,26 +257,25 @@ def route_to_xy(s):
     return dx, dy
 
 
-def create_task(rover, order_chain):
+def create_task(rv, orders):
     task = []
-    if order_chain == []:
+    if orders.chains[rv.idx] == []:
         task = 60*'S'
     else:
-        #for o in order_chain:
-        #    task_tmp = path_to_directions(search(city, tuple(rover.pR), o.pT)) + 'T' + o.path + 'P'
-        #    task.append(task_tmp)
-        o = order_chain.pop(0)
-        print(f"ROVER PATH {rover.idx}@{rover.pR} -> order {o.pT}")
-        task = path_to_directions(search(city, tuple(rover.pR), o.pT)) +'T' + o.path + 'P'
-    #task = "".join(task)
-    return task
+        #o = orders.chains[rv.idx].pop(0)
+        o = orders.pop(rv.idx)
+        task = path_to_directions(search(city, tuple(rv.pR), o.pT)) +'T' + o.path + 'P'
+        #task = 59*'X' + 'P'
+        #print(f"CREATE TASK {rv.idx}@{rv.pR} -> order {o.pT} {task}")
+        rv.update_position(o.pP[0], o.pP[1])
+        rv.task = task
+        rv.state = 'P'
+    #return task
 
 
 def dispatch():
-    #if debug:
-    #    print(city)
-
     orders = Orders(nrovers)
+    global norders
 
     for t in range(nT):
         # 1 get fresh orders
@@ -261,16 +284,14 @@ def dispatch():
             for i in range(n0):
                 o = create_order(get_data())
                 orders.put(o)
-                print(f"{o.pT}->{o.pP}")
-        print(f"iter {t}, orders {n0}")
-        orders.print_chains()
+        #print(f"iter {t}, orders {n0}, max_chain {orders.max_chain()}, empty_chains {orders.empty_chains}")
+        #orders.print_chains(False)
 
         # 2 assign orders to rovers and send commands
         for rv in rvs:
+            #print(f"SHOW ROVER {rv.idx} {rv.state} {rv.pR} {len(orders.chains[rv.idx])}")
             if rv.state == 'S' and len(orders.chains[rv.idx]) > 0:
-                #o = orders.pop(rv.idx)
-                rv.task = create_task(rv, orders.chains[rv.idx])
-                rv.state = 'P'
+                create_task(rv, orders)
 
         # 3 update everything (rovers and orders)
         # orders that are not assigned
@@ -278,24 +299,29 @@ def dispatch():
         #    o.time += 60
         # rovers
         for rv in rvs:
-            #if rv.state == 'P':
-            #   rv.update()
-            #else:
-            #    print(60*"S")
             rv.update()
 
-
+        if time.time() - t0 > 20:
+            break
+        #if debug:
+        #    input()
 
 if __name__ == "__main__":
     # set debug = False before sending solution to yandex
     debug = True
-    task_id = "03"
+    task_id = sys.argv[1] #"04"
+    t0 = time.time()
+    norders = 0
     
     # initialize source
     if debug:
         g = (v for v in open(f"examples/{task_id}"))
 
     N, max_tips, cost_c, city, nT, nD = setup()
+
+    if debug:
+        print(f"task_id {task_id}, nT {nT}, nD {nD}")
+        #input()
 
     nrovers = how_much_rovers(N, max_tips, cost_c)
     print(nrovers) # send data, no remove
@@ -307,3 +333,4 @@ if __name__ == "__main__":
     # receive and process orders
     dispatch()
 
+    print(f"GAME OVER. norders {norders}")
